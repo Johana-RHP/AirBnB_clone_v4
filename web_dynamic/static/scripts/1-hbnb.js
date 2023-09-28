2ae18e7dba75f3b831601d370460f9ba26998f73
Johana-RHP <johana.rosicel@gmail.com>
2023-09-28 18:35:30 -0500

    feat: task 2

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    // Initialize an empty dictionary to store Amenity IDs
+    var amenityIds = {};
+
+    // Listen for changes on each input checkbox tag
+    $('input[type="checkbox"]').change(function() {
+        var amenityId = $(this).closest('li').data('id');
+        var amenityName = $(this).closest('li').data('name');
+
+        if ($(this).is(':checked')) {
+            // Checkbox is checked, add Amenity ID to the dictionary
+            amenityIds[amenityId] = amenityName;
+        } else {
+            // Checkbox is unchecked, remove Amenity ID from the dictionary
+            delete amenityIds[amenityId];
+        }
+
+        // Update the h4 tag inside the div Amenities with the list of checked Amenities
+        var amenitiesList = Object.values(amenityIds).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+});
